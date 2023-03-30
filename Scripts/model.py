01436fea9b3983a7060a8a3609d27c1ad18ac0d8
Máté Horváth <mahte007@gmail.com>
2023-03-30 16:55:55 +0200

    Added data insertion into the database and connection closing function to the database model

--- Scripts/model.py
+++ Scripts/model.py
@@ -12,3 +12,13 @@ class Database:
         column_names = [field['field_name'] for field in fields]
         create_table_sql = f"CREATE TABLE IF NOT EXISTS {table_name} (id INTEGER PRIMARY KEY, {', '.join(column_names)})"
         self.cursor.execute(create_table_sql)
+
+    def add_data(self, data):
+        column_names = [field['field_name'] for field in self.fields]
+        values = tuple(data[column_name] for column_name in column_names)
+        insert_sql = f"INSERT INTO {self.table_name} ({', '.join(column_names)}) VALUES ({', '.join(['?']*len(column_names))})"
+        self.cursor.execute(insert_sql, values)
+        self.connection.commit()
+
+    def close(self):
+        self.connection.close()
